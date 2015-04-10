860506a817b81ccded69671ff823aa1fde112531
ziliang <ziliangq@stanford.edu>
2015-04-09 18:58:52 -0700

    add analyses to url path

--- server/xppfserver/urls.py
+++ server/xppfserver/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^status$', 'apps.controls.views.status'),
+    url(r'^run$', 'apps.controls.views.run'),
     url(r'^analyses$', 'apps.analysis.views.analyses'),
     url(r'^analyses/.*$', 'apps.analysis.views.update'),
 )
