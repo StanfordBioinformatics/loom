980b37be9bf9d181bc4a6607185e1681e7daa625
Isaac Liao <isaacliao@gmail.com>
2015-04-10 03:00:29 +0000

    Refactoring duckling

--- duckling/duckling.py
+++ duckling/duckling.py
@@ -14,49 +14,31 @@ import requests
 import daemon
 
 _DAEMON = False
-_SLEEP_TIME = 5
+_SLEEP_INTERVAL = 5
 _SERVER_URL = 'http://localhost:8000'
 _ANALYSES_URL = _SERVER_URL + '/analyses'
 _STATUS_URL = _SERVER_URL + '/status'
 _STORAGE_ACCOUNT_KEY = '8hz9b5H3broyRlJTxMDFPR2b+LeYrpbD18PZZrbOZ8SNV35IGwL2IXvAgCzJ7qMd4s0LQDqcPS6t+OR4rW6OcQ=='
 _STORAGE_ACCOUNT_NAME = 'scgs'
+_HEADERS = {'Content-type':'application/json','Accept':'text/plain'}
 
-def check_server_status(status_url):
-    r=requests.get(status_url)
-    print(r)
+def check_server_status():
+    r=requests.get(_STATUS_URL)
+    return r
 
-def get_ready_analyses(server_url):
-    """Check the server for new analyses that are ready to run."""
-    # send msg without explict analysis_id, return with todo list
-    r=requests.get(server_url, data={}, headers = {'Content-type':'application/json','Accept':'text/plain'})
-    print(r)
-
-def get_analysis(analysis_id):
-    """Get full JSON specifying the named analysis."""
-    # send msg with explict analysis_id, return analysis description in json format
-    todo_analysis_ids = []
-    query={}
-    if(analysis_id is None):
-        r=requests.get("http://localhost:8000/analyses", data={}, headers = {'Content-type':'application/json','Accept':'text/plain'})
-        query = json.loads( r.text.replace("\\","").replace("]\"","]").replace('\"[',"[") )
-        for q in query:
-            todo_analysis_ids.append(q['fields']['analysis'])
-    else:
-        data_pkg = json.dumps({'analysis_id':analysis_id})
-        r=requests.get("http://localhost:8000/analyses", data=data_pkg, headers = {'Content-type':'application/json','Accept':'text/plain'})
-        query = json.loads( r.text.replace("\\","").replace("]\"","]").replace('\"[',"[") )
-    return query
+def get_ready_analyses():
+    """Get all ready analyses."""
+    r=requests.get(_ANALYSES_URL, params={'status':'ready'}) 
+    json_response = r.json()
+    return json_response
 
 def run_analysis(analysis):
     """Run the analysis."""
-    analysis_json = json.loads(open("analysis_demo.json").read())
-    r=requests.post(SERVER_URL, data=analysis_josn, headers = {'Content-type':'application/json','Accept':'text/plain'})
 
-def update_status(server_url):
-    """Update the server with the current status of each analysis? all analyses?"""
-    analysis_id = "93eef400-ddb5-11e4-ab2d-60f81dd0008a"
-    status_data = '{"analysis_id":"'+analysis_id+'","server":"localhost"}'
-    r=requests.post("http://localhost:8000/"+analysis_id, data={}, headers = {'Content-type':'application/json','Accept':'text/plain'})
+def update_status(analysis_id, status):
+    """Update the server with the current status of an analysis."""
+    data_pkg = json.dumps({'status':status})
+    r=requests.update(_ANALYSES_URL + '/' + str(analysis_id), data=data_pkg, headers = _HEADERS)
 
 def download_file(remotecontainer, remoteblob, localfile):
     transfer_file(localfile, remotecontainer, remoteblob)
@@ -70,7 +52,7 @@ def transfer_file(localfile, remotecontainer, remoteblob):
 
 def main():
     """Runs as a detached process, writing stdout and stderr to log files in the current directory."""
-    if DAEMON:
+    if _DAEMON:
         daemon_context = daemon.DaemonContext(
             working_directory='.',
             stdout=open('out.log', 'w'),
@@ -79,23 +61,32 @@ def main():
         daemon_context.open()
 
     while(True):
-        analysis_status = get_analysis(None)
-        print( "#"+str(analysis_status)+" of analyses in the todo list" )
-        for analysis_entry in analysis_status:
-            #Execute the analysis
-            analyses = get_analysis(analysis_entry['fields']['analysis'])
-            print("\n***analyze:"+str(analyses))
-            step_i=0
-            for analysis in analyses:
-                step_i = step_i+1
-                container = analysis['container']
-                command = analysis['command']
-                print(">>>STEP "+str(step_i))
-                print("container:"+container)
-                print("command:"+command)
-            #update_analysis(analysis_id)
-        time.sleep(1) 
-        print('hi')
+        # Check server status
+        current_status = check_server_status()
+        print(current_status)
+
+        # Check for ready analyses
+        analyses = get_ready_analyses()        
+        print(analyses)
+        print(len(analyses), 'analyses ready')
+
+        # Run ready analyses
+#         for analysis in analyses:
+#             analyses = get_analysis(analysis_entry['fields']['analysis'])
+#             print("\n***analyze:"+str(analyses))
+#             step_i=0
+#             for analysis in analyses:
+#                 step_i = step_i+1
+#                 container = analysis['container']
+#                 command = analysis['command']
+#                 print(">>>STEP "+str(step_i))
+#                 print("container:"+container)
+#                 print("command:"+command)
+
+        # Update server with status of each analysis
+
+        # Go back to sleep
+        time.sleep(_SLEEP_INTERVAL) 
 
 #    while(True):
 #        print('Sleeping for', SLEEP_TIME, 'seconds')
