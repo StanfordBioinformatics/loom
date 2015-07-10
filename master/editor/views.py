329a944d32d98cd6b0c86b73cfa67ccdb0cf179a
Nathan Hammond <nathan.hammond@gmail.com>
2015-07-10 00:00:01 -0700

    renamed server to master

--- master/editor/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    html = "<html><body>It works!</body></html>"
-    return render(request, 'editor/index.html')
