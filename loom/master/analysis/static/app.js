de1b3fa3c7e64442d8d6283ccf51a33f35651f80
isaacliao <isaacliao@gmail.com>
2016-02-23 17:59:41 -0800

    Workflows and workflow detail views; breadcrumb nav works.

--- loom/master/analysis/static/app.js
+++ loom/master/analysis/static/app.js
@@ -1,19 +1,30 @@
 var loomApp = angular.module('loomApp', [
-    'ngRoute',
+    'ui.router',
     'loomControllers']);
 
-loomApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/workflows', {
-        templateUrl: 'partials/workflow-list.html',
-        controller: 'WorkflowListCtrl'
-      }).
-      when('/workflows/:workflowId', {
-        templateUrl: 'partials/workflow-detail.html',
-        controller: 'WorkflowDetailCtrl'
-      }).
-      otherwise({
-        redirectTo: '/workflows'
-      });
-  }]);
+loomApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/workflows');
+
+    $stateProvider
+        .state('workflows', {
+            url:'/workflows',
+            templateUrl: 'partials/workflow-list.html',
+            controller: 'WorkflowListCtrl'
+        })
+        .state('workflows.workflow', {
+            url:'/{workflowId}',
+            templateUrl: 'partials/workflow-detail.html',
+            controller: 'WorkflowDetailCtrl'
+        })
+        .state('workflows.workflow.step', {
+            url:'/{stepId}', 
+            templateUrl: 'partials/step-detail.html',
+            controller: 'StepDetailCtrl'
+        });
+}]);
+
+// Define a service which we will use to pass data between controllers
+loomApp.factory('$loom', function() {
+    var loomServiceScope = {};
+    return loomServiceScope;
+});
--- loom/master/analysis/static/controllers.js
+++ loom/master/analysis/static/controllers.js
@@ -1,19 +1,35 @@
 var loomControllers = angular.module('loomControllers', []);
 
-loomControllers.controller('WorkflowListCtrl', ['$scope', '$http',
-    function ($scope, $http) {
+loomControllers.controller('BreadcrumbCtrl', ['$scope', '$loom', '$state',
+    function ($scope, $loom, $state) {
+        $scope.$state = $state;
+        $scope.$loom = $loom;
+    }]);
+
+loomControllers.controller('WorkflowListCtrl', ['$scope', '$http', '$loom', '$state',
+    function ($scope, $http, $loom, $state) {
         // Query the Loom server API for workflows. Assumes same server.
         // May need to watch out for same-origin policy / cross-site scripting restrictions if
         // this page is hosted in a different location than the Loom server.
-        $http.get('http://' + window.location.host + '/api/workflows').success(function(data) {
-            $scope.workflows = data;
-            //console.log('got workflows:');
-            //console.log(data);
+        $http.get('http://' + window.location.host + '/api/workflows').success(function(response) {
+            $loom.workflows = response['workflows'];
+            $scope.workflows = $loom.workflows;
+        });
+        $scope.$state = $state;
+    }]);
+
+loomControllers.controller('WorkflowDetailCtrl', ['$scope', '$http', '$loom', '$stateParams', 
+    function($scope, $http, $loom, $stateParams) {
+        $http.get('http://' + window.location.host + '/api/workflows/' + $stateParams.workflowId).success(function(response) {
+            $loom.workflow = response;
+            $scope.workflow = $loom.workflow;
         });
     }]);
 
-loomControllers.controller('WorkflowDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.workflowId = $routeParams.workflowId;
-  }]);
-
+loomControllers.controller('StepDetailCtrl', ['$scope', '$http', '$loom', '$stateParams',
+    function($scope, $http, $loom, $stateParams) {
+        $http.get('http://' + window.location.host + '/api/steps/' + $stateParams.stepId).success(function(response) {
+            $loom.step = response;
+            $scope.stepId = $stateParams.stepId;
+        });
+    }]);
