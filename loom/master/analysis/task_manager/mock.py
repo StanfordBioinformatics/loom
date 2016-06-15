e6ecc1196f38e8ad13f748279034d17ccd7103b8
Nathan Hammond <nathan.hammond@gmail.com>
2016-06-15 11:24:38 -0700

    update mock task manager

--- loom/master/analysis/task_manager/mock.py
+++ loom/master/analysis/task_manager/mock.py
@@ -14,34 +14,36 @@ class MockTaskManager(object):
     def run(cls, task_run):
         from analysis.models.task_runs import MockTaskRunAttempt
         attempt = MockTaskRunAttempt.create(
-            {'task_run': task_run.to_struct()})
+            {'task_run': task_run})
 
         for output in attempt.outputs.all():
-            data_object_struct = cls._create_mock_file_data_object(output)
-            cls._mock_upload(output)
+            file_data_object = cls._create_mock_file_data_object(output)
+            cls._mock_upload(file_data_object)
+            output.update({})
 
     @classmethod
-    def _mock_upload(cls, output):
-        output.refresh_from_db()
-        output_struct = output.to_struct()
-        output_struct['file_location']['status'] = 'complete'
-        output_struct['temp_file_location'] = None
-        output.update(output_struct)
+    def _mock_upload(cls, data_object):
+        data_object_struct = data_object.to_struct()
+        data_object_struct['file_import']['temp_file_location'] = None
+        data_object_struct['file_import']['file_location']['status'] = 'complete'
+        data_object.update(data_object_struct)
+        
 
     @classmethod
     def _create_mock_file_data_object(self, output):
         self.mock_file_counter += 1
         mock_text = "mock%s" % self.mock_file_counter
         mock_md5 = hashlib.md5(mock_text)
-        return FileDataObject.create(
-            {
-                'file_content':{
-                    'filename': mock_text,
-                    'unnamed_file_content': {
-                        'hash_value': mock_md5.hexdigest(),
-                        'hash_function': 'md5',
-                    }
-                },
-                'file_import': output
-            }
-        )
+        output.update({
+            'data_object': {
+                'file_import': {
+                    '_class': 'TaskRunAttemptOutputFileImport'
+                }}})
+        output.data_object.update({
+            'file_content':{
+                'filename': mock_text,
+                'unnamed_file_content': {
+                    'hash_value': mock_md5.hexdigest(),
+                    'hash_function': 'md5',
+                }}})
+        return output.data_object
