5c2cb91f420f9138d999da892fd8ac46f475344d
Nathan Hammond <nathan.hammond@gmail.com>
2017-02-13 23:53:19 -0800

    handle gce service account json key

---
+++ loomengine/client/inventory/gce_wrapper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import os
+import subprocess
+import copy
+import json
+import sys
+
+inventory_file = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)),
+                              'gce.py')
+
+env = copy.deepcopy(os.environ)
+
+def get_required_setting(setting):
+    try:
+        return env[setting]
+    except KeyError:
+        raise Exception('Missing setting %s' % setting)
+
+LOOM_SETTINGS_HOME = get_required_setting('LOOM_SETTINGS_HOME')
+LOOM_ADMIN_FILES_DIR = get_required_setting('LOOM_ADMIN_FILES_DIR')
+LOOM_GCE_PEM_FILE = get_required_setting('LOOM_GCE_PEM_FILE')
+
+GCE_PEM_FILE_PATH = os.path.join(LOOM_SETTINGS_HOME, LOOM_ADMIN_FILES_DIR, LOOM_GCE_PEM_FILE)
+
+with open(GCE_PEM_FILE_PATH, 'r') as f:
+    key_json = json.load(f)
+
+GCE_EMAIL = key_json.get('client_email')
+GCE_PROJECT = key_json.get('project_id')
+
+
+# get pem file path from variables "settings-dir", "admin-files", "gce_pem_file"
+# extract email
+# extract project
+
+env.update({
+    'GCE_PROJECT': GCE_PROJECT,
+    'GCE_PEM_FILE_PATH': GCE_PEM_FILE_PATH,
+    'GCE_EMAIL': GCE_EMAIL,
+})
+
+print subprocess.check_output([inventory_file], env=env)
+sys.exit(0)
