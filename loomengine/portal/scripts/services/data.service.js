35443ab2deb1fca9c1f44efd21c99edb4edf6ac7
isaacliao <isaacliao@gmail.com>
2017-05-05 15:03:51 -0700

    Groundwork for viewing task attempts in dashboard.

--- loomengine/portal/scripts/services/data.service.js
+++ loomengine/portal/scripts/services/data.service.js
@@ -1,10 +1,10 @@
-'use strict';
+"use strict";
 
 angular
-    .module('loom.services')
-    .service('DataService', DataService)
+.module("loom.services")
+.service("DataService", DataService)
 
-DataService.$inject = ['$http', '$q'];
+DataService.$inject = ["$http", "$q"];
 
 function DataService($http, $q) {
     /* DataService retrieves and caches data from the server. */
@@ -24,121 +24,146 @@ function DataService($http, $q) {
     var activeData = {};
 
     function getAllActive() {
-	return activeData;
-    };
+        return activeData;
+    }
 
     function setActiveRun(runId) {
-	return $http.get('/api/runs/' + runId + '/')
-            .then(function(response) {
-		activeData.run = response.data;
-		expandRunInputsOutputs();
-        expandRunTasks();
-	    });
-    };
+        return $http.get("/api/runs/" + runId + "/")
+        .then(function(response) {
+            activeData.run = response.data;
+            expandRunInputsOutputs();
+            if (activeData.run.tasks !== undefined) {
+                expandRunTasks();
+            }
+        });
+    }
 
     function setActiveTaskAttempt(taskAttemptId) {
-	return $http.get('/api/task-attempts/' + taskAttemptId + '/')
-            .then(function(response) {
-		activeData.taskAttempt = response.data;
-	    });
-    };
+        return $http.get("/api/task-attempts/" + taskAttemptId + "/")
+        .then(function(response) {
+            activeData.taskAttempt = response.data;
+        });
+    }
 
     function expandRunInputsOutputs() {
-	for (var i=0; i < activeData.run.inputs.length; i++) {
-	    expandRunInput(i);
-	}
-	for (var i=0; i < activeData.run.outputs.length; i++) {
-	    expandRunOutput(i);
-	}
-    };
+        for (var i=0; i < activeData.run.inputs.length; i++) {
+            expandRunInput(i);
+        }
+        for (var i=0; i < activeData.run.outputs.length; i++) {
+            expandRunOutput(i);
+        }
+    }
 
     function expandRunInput(i) {
-	return $http.get('/api/data-trees/'+activeData.run.inputs[i].data.uuid)
-	    .then(function(response) {
-		activeData.run.inputs[i].data = response.data;
-	    });
-    };
+        return $http.get("/api/data-trees/"+activeData.run.inputs[i].data.uuid)
+        .then(function(response) {
+            activeData.run.inputs[i].data = response.data;
+        });
+    }
 
     function expandRunOutput(i) {
-	return $http.get('/api/data-trees/'+activeData.run.outputs[i].data.uuid)
-	    .then(function(response) {
-		activeData.run.outputs[i].data = response.data;
-	    });
-    };
+        return $http.get("/api/data-trees/"+activeData.run.outputs[i].data.uuid)
+        .then(function(response) {
+            activeData.run.outputs[i].data = response.data;
+        });
+    }
 
     function expandRunTasks() {
-	for (var i=0; i < activeData.run.tasks.length; i++) {
-        return $http.get('/api/tasks/'+activeData.run.tasks[i].uuid)
-            .then(function(response) {
+        for (var i=0; i < activeData.run.tasks.length; i++) {
+            expandRunTask(i);
+            if (activeData.run.tasks[i].task_attempts !== undefined) {
+                expandRunTaskTaskAttempts(i);
+            }
+        }
+    }
+
+    function expandRunTask(i) {
+        return $http.get("/api/tasks/"+activeData.run.tasks[i].uuid)
+        .then(function(response) {
             activeData.run.tasks[i] = response.data;
-            });
-    };
+        });
+    }
+
+    function expandRunTaskTaskAttempts(i) {
+        for (var j=0; j < activeData.run.tasks[i].task_attempts.length; j++) {
+            expandRunTaskTaskAttempt(i, j);
+        }
+    }
+
+    function expandRunTaskTaskAttempt(i, j) {
+        return $http.get("/api/task-attempts/"+activeData.run.tasks[i].task_attempts[j].uuid)
+        .then(function(response) {
+            activeData.run.tasks[i].task_attempts[j] = response.data;
+        });
+    }
 
     function setActiveTemplate(templateId) {
-	return $http.get('/api/templates/' + templateId + '/')
-            .then(function(response) {
-		activeData.template = response.data;
-		expandTemplateInputs();
-            });
-    };
+        return $http.get("/api/templates/" + templateId + "/")
+        .then(function(response) {
+            activeData.template = response.data;
+            expandTemplateInputs();
+        });
+    }
 
     function expandTemplateInputs() {
-	for (var i=0; i < activeData.template.fixed_inputs.length; i++) {
-	    expandTemplateFixedInput(i);
-	}
-    };
+        for (var i=0; i < activeData.template.fixed_inputs.length; i++) {
+            expandTemplateFixedInput(i);
+        }
+    }
 
     function expandTemplateFixedInput(i) {
-	return $http.get('/api/data-trees/'+activeData.template.fixed_inputs[i].data.uuid)
-	    .then(function(response) {
-		activeData.template.fixed_inputs[i].data = response.data;
-	    });
-    };
+        return $http.get("/api/data-trees/"+activeData.template.fixed_inputs[i].data.uuid)
+        .then(function(response) {
+            activeData.template.fixed_inputs[i].data = response.data;
+        });
+    }
 
     function setActiveFile(fileId) {
-	return $http.get('/api/data-files/' + fileId + '/')
-            .then(function(response) {
-		activeData.file = response.data;
-            });
-    };
+        return $http.get("/api/data-files/" + fileId + "/")
+        .then(function(response) {
+            activeData.file = response.data;
+        });
+    }
 
     function getFileProvenance(fileId) {
-	return $http.get('/api/data-files/' + fileId + '/provenance/')
-	    .then(function(response) {
-		return response.data.provenance;
-	    });
-    };
+        return $http.get("/api/data-files/" + fileId + "/provenance/")
+        .then(function(response) {
+            return response.data.provenance;
+        });
+    }
 
     function getRuns() {
-	return $http.get('/api/runs/?parent_only')
-	    .then(function(response) {
-		return response.data;
-	    });
-    };
+        return $http.get("/api/runs/?parent_only")
+        .then(function(response) {
+            return response.data;
+        });
+    }
 
     function getTemplates() {
-	return $http.get('/api/templates/?imported')
-	    .then(function(response) {
-		return response.data;
-	    });
-    };
+        return $http.get("/api/templates/?imported")
+        .then(function(response) {
+            return response.data;
+        });
+    }
 
     function getImportedFiles() {
-	return $http.get('/api/data-files/?source_type=imported')
-	    .then(function(response) {
-		return response.data;
-	    });
-    };
+        return $http.get("/api/data-files/?source_type=imported")
+        .then(function(response) {
+            return response.data;
+        });
+    }
+
     function getResultFiles() {
-	return $http.get('/api/data-files/?source_type=result')
-	    .then(function(response) {
-		return response.data;
-	    });
-    };
+        return $http.get("/api/data-files/?source_type=result")
+        .then(function(response) {
+            return response.data;
+        });
+    }
+
     function getLogFiles() {
-	return $http.get('/api/data-files/?source_type=log')
-	    .then(function(response) {
-		return response.data;
-	    });
-    };
-};
+        return $http.get("/api/data-files/?source_type=log")
+        .then(function(response) {
+            return response.data;
+        });
+    }
+}
