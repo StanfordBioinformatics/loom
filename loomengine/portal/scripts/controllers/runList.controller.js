029cae88bb8467b8946db0ae6962e8fe73f8d663
isaacliao <isaacliao@gmail.com>
2017-05-11 18:34:33 -0700

    Run list displays step/task/taskattempt hierarchy and links to logs for task attempts.

--- loomengine/portal/scripts/controllers/runList.controller.js
+++ loomengine/portal/scripts/controllers/runList.controller.js
@@ -4,9 +4,10 @@ angular
     .module('loom.controllers')
     .controller('RunListController', RunListController);
 
-RunListController.$inject = ['$scope', 'DataService'];
+RunListController.$inject = ['$scope', 'DataService', '$location'];
 
-function RunListController($scope, DataService) {
+function RunListController($scope, DataService, $location) {
+    $scope.$location = $location;
     $scope.loading = true;
     DataService.getRunWorkflowsExpanded().then(function(runs) {
 	    $scope.loading = false;
