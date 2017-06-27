f6cf8aafe4ee5cedb58706e8c6fb9818befd8640
Nathan Hammond <nathan.hammond@gmail.com>
2017-06-27 11:29:22 -0700

    loomData directive to render nested data in browser

---
+++ loomengine/portal/scripts/directives/loomData.directive.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular
+    .module('loom.directives')
+    .directive('loomData', loomData)
+
+function loomData($compile){
+    return {
+	restrict: "E",
+	replace: true,
+	scope: {
+	    contents: '='
+	},
+	template: "<span><a ng-if='contents.type==\"file\"' href='#/files/{{contents.uuid}}'>{{contents.value.filename}}@{{contents.uuid|limitTo:8}}</a>{{ (contents.value && contents.type != 'file') ? contents.value : null }}<ul ng-if='!contents.value'><li ng-repeat='child in contents'>&nbsp;<loom-data contents='child'></loom-data></li><ul></span>"
+    }
+}
