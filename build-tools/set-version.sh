e6181e6e99d1fc45c1ee2b529ed19ad796e1241c
Nathan Hammond <nathan.hammond@gmail.com>
2018-08-27 19:29:07 -0700

    Prerelease 0.7.0 (#551)
    * update release notes for 0.7.0
    
    * fix PyPi test, doc version
    
    * update jenkins configuration
    
    * correct typo in Jenkinsfile

--- build-tools/set-version.sh
+++ build-tools/set-version.sh
@@ -35,16 +35,6 @@ echo Updating version to \"$version\"
 echo "    updating $DIR/../VERSION"
 echo $version > $DIR/../VERSION
 
-if [ -f $DIR/../doc/conf.py ]; then
-  echo "    updating $DIR/../doc/conf.py"
-  sed -i.tmp "s/version = u'.*'/version = u'$version'/" $DIR/../doc/conf.py
-  sed -i.tmp "s/release = u'.*'/release = u'$version'/" $DIR/../doc/conf.py
-  rm $DIR/../doc/conf.py.tmp
-else
-  echo "    skipping $DIR/../doc/conf.py ... does not exist"
-fi
-
-
 for package_dir in $DIR/../utils/loomengine_utils $DIR/../worker/loomengine_worker $DIR/../server/loomengine_server $DIR/../client/loomengine
 
 do
